26b54d51e458ce681d97e56c9bd8ffa7476de685
bottae <bottae@gmail.com>
2016-04-08 16:48:39 +0900

    Final

--- MiniBrowser/ViewController.swift
+++ MiniBrowser/ViewController.swift
@@ -8,22 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate, UIWebViewDelegate {
 
-    @IBOutlet weak var bookMarkSegmentedControl: UISegmentedControl!
+    @IBOutlet var bookMarkSegmentedControl: UISegmentedControl!
     
-    @IBOutlet weak var urlTextField: UITextField!
-
-    @IBOutlet weak var mainWebView: UIWebView!
+    @IBOutlet var urlTextField: UITextField!
+    
+    @IBOutlet var mainWebView: UIWebView!
+    
+    @IBOutlet var spinningActivityIndicatorView: UIActivityIndicatorView!
     
     
     @IBAction func bookMarkAction(sender: AnyObject) {
         
+        let bookMarkUrl = bookMarkSegmentedControl.titleForSegmentAtIndex(bookMarkSegmentedControl.selectedSegmentIndex)
+        
+        let urlString = "http://www.\(bookMarkUrl!).com"
+        
+        mainWebView.loadRequest(NSURLRequest(URL: NSURL(string: urlString)!))
+        
+    }
+    
+    func webViewDidStartLoad(webView: UIWebView) {
+        spinningActivityIndicatorView.startAnimating()
+    }
+    
+    func webViewDidFinishLoad(webView: UIWebView) {
+        spinningActivityIndicatorView.stopAnimating()
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        var urlString = "\(urlTextField.text!)"
+        if !urlString.hasPrefix("http://") {
+            urlString = "http://\(urlTextField.text!)"
+        }
+        mainWebView.loadRequest(NSURLRequest(URL: NSURL(string: urlString)!))
+        textField.resignFirstResponder()
+        return true
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let urlString = "http://www.facebook.com"
+        mainWebView.loadRequest(NSURLRequest(URL: NSURL(string: urlString)!))
+        urlTextField.text = urlString
+        
     }
 
     override func didReceiveMemoryWarning() {
